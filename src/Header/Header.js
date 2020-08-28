1a5b5cfbd11495b52291adb856d19be1040d588f
ahhzaky <siahmadzaky@gmail.com>
2020-08-28 22:58:56 +0700

    create header

---
+++ src/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return <div clasName="header">
+            <div className="header_left">
+
+            {/* avatars for loged */}
+            {/* time icon */}
+            </div>
+            <div className="header_search">
+                {/* search icon */}
+                {/* input */}
+            </div>
+            <div className="header__right">
+                {/* help icon */}
+            </div>
+        </div>;
+}
+
+export default Header;
