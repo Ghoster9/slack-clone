b6402a0c8e8d2529225017e33c597fcff60ddbd0
ahhzaky <siahmadzaky@gmail.com>
2020-08-28 23:39:48 +0700

    header search responsive

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 
 function Header() {
-    return <div clasName="header">
-            <div className="header_left">
-
-            {/* avatars for loged */}
-            {/* time icon */}
-            </div>
-            <div className="header_search">
-                {/* search icon */}
-                {/* input */}
-            </div>
-            <div className="header__right">
-                {/* help icon */}
-            </div>
-        </div>;
+  return (
+    <div className="header">
+      <div className="header__left">
+        <Avatar className="header__avatar" alt="ahmad zaky" src="" />
+        <AccessTimeIcon />
+      </div>
+      <div className="header__search">
+        <SearchIcon />
+        <input placeholder="Search Ahamd Zaky" type="text" />
+      </div>
+      <div className="header__right">
+        <HelpOutlineIcon />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
