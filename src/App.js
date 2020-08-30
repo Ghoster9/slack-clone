d99f74fb088c0351a1d8e26e105abeabf1c36159
ahhzaky <siahmadzaky@gmail.com>
2020-08-30 15:08:09 +0700

    db add channel sidebar and sidebarOption

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import './App.css';
-import Header from './Header/Header';
-import Sidebar from './Sidebar/Sidebar';
+import React from "react";
+import "./App.css";
+import Header from "./Header/Header";
+import Sidebar from "./Sidebar/Sidebar";
+import Chat from "./Chat/Chat";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>HELLO jack programer</h1>
-      <Header />
-      <div className="app__body">
-        <Sidebar/>
-        {/* sidebar */}
-        {/* react-router chatscren */}
-      </div>
+      <Router>
+        <h1>HELLO jack programer</h1>
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/room/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <h1> welcome </h1>
+            </Route>
+          </Switch>
+          {/* react-router chatscren */}
+        </div>
+      </Router>
     </div>
   );
 }
--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -52,7 +52,7 @@ function Sidebar() {
       <hr />
       <SidebarOption Icon={ExpandMoreIcon} title="Channels" />
       <hr />
-      <SidebarOption Icon={AddIcon} title="Add Channel" />
+      <SidebarOption Icon={AddIcon} addChannelOption title="Add Channel" />
 
       {/* connec to db and list all the channels */}
       {/* sidebarOption */}
